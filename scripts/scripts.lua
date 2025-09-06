95623aeeee3b68020704ce19cd4ca778fcd68596
March012847 <tur830106@tkt.ac.th>
2025-09-06 16:02:09 +0700

    Create scripts.lua

---
+++ scripts/scripts.lua
@@ -0,0 +1,6 @@
+return {
+  ping = {
+    script1 = "https://raw.githubusercontent.com/March012847/Cmdr_fetched/refs/heads/main/scripts/ping/ping.lua",
+    script2 = "https://raw.githubusercontent.com/March012847/Cmdr_fetched/refs/heads/main/scripts/ping/pingserver.lua",
+  }
+}
