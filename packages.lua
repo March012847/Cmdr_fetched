917e2a65bd4676721dad46531fa154b84ef8d059
March012847 <tur830106@tkt.ac.th>
2025-09-06 16:02:39 +0700

    Update packages.lua

--- packages.lua
+++ packages.lua
@@ -2,6 +2,6 @@ return {
   ping = {
     version = "0.0.1",
     side = "Server",
-    url = "placeholder"
+    url = "https://raw.githubusercontent.com/March012847/Cmdr_fetched/refs/heads/main/scripts/scripts.lua"
   },
 }
